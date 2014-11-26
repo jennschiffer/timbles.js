9589764837c4db6c06c7d05f6457f80a09862bab
Jenn Schiffer <jenn@pancaketheorem.com>
2014-11-26 18:55:24 -0500

    add a pretty shitty test for no-sort

--- tests/tests.js
+++ tests/tests.js
@@ -12,3 +12,10 @@ QUnit.test( "Correct Number of Non-header Record Rows", function( assert ) {
   var numRows = $('table').find('tr').not('.header-row').length;
   assert.ok( numRows == "8", "Passed!" );
 });
+
+QUnit.test( "Header with no-sort class is not sortable", function( assert ) {
+  var $noSortColumns = $('.no-sort');
+  $noSortColumns.click();
+  
+  assert.ok( !$noSortColumns.hasClass('sorted-asc') && !$noSortColumns.hasClass('sorted-desc'), "Passed!" );
+});
