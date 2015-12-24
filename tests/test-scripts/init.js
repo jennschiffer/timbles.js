14de8e9292e01e195c79ca7f8a336b0ca2c559fe
Elmer de Looff <elmer.delooff@gmail.com>
2015-12-24 11:19:59 +0100

    Cleaned up init tests
    - Replaced .ok() with .equal() to verify equality;
    - Removed descriptions of tests that stated a success message.

--- tests/test-scripts/init.js
+++ tests/test-scripts/init.js
@@ -1,16 +1,16 @@
 /* testing initialization of timbles tables */
 
-QUnit.test( 'Correct Number of Table Rows', function( assert ) {
+QUnit.test('Correct Number of Table Rows', function(assert) {
   var numRows = $('table').find('tr').length;
-  assert.ok( numRows === 9, 'Passed!' );
-}); 
+  assert.equal(numRows, 9);
+});
 
-QUnit.test( 'Detect single Header Row', function( assert ) {
+QUnit.test('Detect single Header Row', function(assert) {
   var numRows = $('table').find('tr.header-row').length;
-  assert.ok( numRows === 1, 'Passed!' );
-}); 
+  assert.equal(numRows, 1);
+});
 
-QUnit.test( 'Correct Number of Non-header Record Rows', function( assert ) {
+QUnit.test('Correct Number of Non-header Record Rows', function(assert) {
   var numRows = $('table').find('tr').not('.header-row').length;
-  assert.ok( numRows === 8, 'Passed!' );
+  assert.equal(numRows, 8);
 });
