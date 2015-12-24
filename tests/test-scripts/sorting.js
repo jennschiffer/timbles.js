cb2528c9c90bd901d7a45fee7c5b9239d3002f78
Elmer de Looff <elmer.delooff@gmail.com>
2015-12-24 18:17:44 +0100

    Added explicit sorting tests for initial suite.

--- tests/test-scripts/sorting.js
+++ tests/test-scripts/sorting.js
@@ -2,6 +2,14 @@
 
 var target = $('#target');
 
+function sortedColumnContent(columnIndex, order) {
+  // Sorts by the given column and returns an array with the column contents
+  target.timbles('sortColumn', columnIndex, order || 'asc');
+  return target.find('tbody tr').map(function () {
+    return $(this).children().eq(columnIndex).text()
+  }).get();
+}
+
 QUnit.test('Clicking a column header sorts table by that column', function( assert) {
   var $firstColumnHeader = target.find('thead tr th').eq(0);
   assert.ok(!$firstColumnHeader.hasClass('sorted-asc'), 'Not pre-sorted');
@@ -34,4 +42,52 @@ QUnit.test('There is one unsortable header and sorting by it does nothing', func
   $noSortColumns.click();
   assert.ok(!$noSortColumns.hasClass('sorted-asc'), 'Not sorted ascending');
   assert.ok(!$noSortColumns.hasClass('sorted-desc'), 'Not sorted descending');
+});
+
+QUnit.test('Sorting names with case as written', function(assert) {
+  var expected = [
+      'Arthur Rimbaud',
+      'Arthur fforde',
+      'Ezra Pound',
+      'Robert Frost',
+      'e e cummings',
+  ];
+  assert.deepEqual(
+      sortedColumnContent(0), expected, 'Ascending order');
+  expected.reverse();
+  assert.deepEqual(
+      sortedColumnContent(0, 'desc'), expected, 'Descending order');
+});
+
+QUnit.test('Sorting names as lowercase data-values', function(assert) {
+  var expected = [
+      'Arthur fforde',
+      'Arthur Rimbaud',
+      'e e cummings',
+      'Ezra Pound',
+      'Robert Frost',
+  ];
+  assert.deepEqual(
+      sortedColumnContent(1), expected, 'Ascending order');
+  expected.reverse();
+  assert.deepEqual(
+      sortedColumnContent(1, 'desc'), expected, 'Descending order');
+});
+
+QUnit.test('Sorting numbers lexicographically', function(assert) {
+  var expected = ['11', '120', '19', '5', '500'];
+  assert.deepEqual(
+      sortedColumnContent(2), expected, 'Ascending order');
+  expected.reverse();
+  assert.deepEqual(
+      sortedColumnContent(2, 'desc'), expected, 'Descending order');
+});
+
+QUnit.test('Sorting numbers naturally', function(assert) {
+  var expected = ['5', '11', '19', '120', '500'];
+  assert.deepEqual(
+      sortedColumnContent(3), expected, 'Ascending order');
+  expected.reverse();
+  assert.deepEqual(
+      sortedColumnContent(3, 'desc'), expected, 'Descending order');
 });
