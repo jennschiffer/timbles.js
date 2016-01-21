dfaaebb2c29e15eab711f9ff0fbb1f9d18c40f38
Elmer de Looff <elmer.delooff@gmail.com>
2016-01-22 00:24:52 +0100

    Include test scripts gruntfile itself to validation targets.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,10 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      all: 'timbles.js'
+      all: ['Gruntfile.js', 'timbles.js', 'tests/test-scripts/*.js']
     },
     jscs: {
-      src: 'timbles.js',
+      src: ['Gruntfile.js', 'timbles.js', 'tests/test-scripts/*.js'],
       options: {
         config: '.jscsrc',
         verbose: true // Create output with rule names
