c69d7cea35737c7c392286fd286dd0ba20a720ba
Elmer de Looff <elmer.delooff@gmail.com>
2016-01-22 00:36:48 +0100

    Fix style errors in gruntfile and test scripts.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,24 @@
-module.exports = function(grunt) {
+module.exports = function( grunt ) {
 
   // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig( {
+    pkg: grunt.file.readJSON( 'package.json' ),
     jshint: {
-      all: ['Gruntfile.js', 'timbles.js', 'tests/test-scripts/*.js']
+      all: [ 'Gruntfile.js', 'timbles.js', 'tests/test-scripts/*.js' ]
     },
     jscs: {
-      src: ['Gruntfile.js', 'timbles.js', 'tests/test-scripts/*.js'],
+      src: [ 'Gruntfile.js', 'timbles.js', 'tests/test-scripts/*.js' ],
       options: {
         config: '.jscsrc',
         verbose: true // Create output with rule names
       }
     }
-  });
+  } );
 
   // Load the JSHint and JSCS plugins.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-jscs');
+  grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+  grunt.loadNpmTasks( 'grunt-jscs' );
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint', 'jscs']);
+  grunt.registerTask( 'default', [ 'jshint', 'jscs' ] );
 };
