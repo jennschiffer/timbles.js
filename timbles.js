44f1066d3dc4991722bfade6fea9f96825559157
Elmer de Looff <elmer.delooff@gmail.com>
2016-02-27 23:47:59 +0100

    Updated setupExistingTable to select header columns for multi-row tables.
    This is done by constructing the full grid of header cells, resolving row/column
    spans. After this, the newly constructed grid is inspected to select the best header
    for each column. These are then concatenated into a new jQuery object of headers.

--- timbles.js
+++ timbles.js
@@ -86,12 +86,53 @@ var methods = {
   },
 
   setupExistingTable: function() {
+    var $cell, col, colOffset, colSpan, row, rowOffset, rowSpan;
     var data = this.data( pluginName );
+    var $headerRows = this.find( 'thead tr' );
+    var headerCells = {};
+    var colCount = 0;
 
-    // Select column headers and add column index to them
-    data.$headers = this.find( 'thead th' ).each( function( index ) {
-      $( this ).data( 'timbles-column-index', index );
+    // Resolve col/row-spans and determine column header candidates
+    $headerRows.each( function( rowIndex ) {
+      $( this ).find( 'th' ).each( function( colIndex ) {
+        $cell = $( this );
+
+        // Rowspans cause cells to be in a different place than the DOM suggests
+        while ( headerCells.hasOwnProperty( [ rowIndex, colIndex ] ) ) {
+          colIndex++;
+        }
+
+        // Determine cell size and update table column count
+        rowSpan = parseInt( $cell.attr( 'rowspan' ) ) || 1;
+        colSpan = parseInt( $cell.attr( 'colspan' ) ) || 1;
+        colCount = Math.max( colIndex + colSpan, colCount );
+
+        // Add cell to all 'grid spots' it occupies so we can detect spans
+        // Cells that span multiple columns are never a column header
+        $cell = ( colSpan === 1 ) ? $cell : null;
+        for ( rowOffset = 0; rowOffset < rowSpan; rowOffset++ ) {
+          for ( colOffset = 0; colOffset < colSpan; colOffset++ ) {
+            headerCells[ [ rowIndex + rowOffset, colIndex + colOffset ] ] = $cell;
+          }
+        }
+      } );
     } );
+
+    // Determine actual column headers, search header rows bottom to top
+    var headers = [];
+    for ( col = 0; col < colCount; col++ ) {
+      for ( row = $headerRows.length; row-- > 0; ) {
+        $cell = headerCells[ [ row, col ] ];
+        if ( $cell !== null ) {
+          $cell.data( 'timbles-column-index', col );
+          headers.push( $cell.get( 0 ) );
+          break;
+        }
+      }
+    }
+
+    // Create jQuery object from selected column headers
+    data.$headers = $( headers );
 
     // Start enabling any given features
     methods.enableFeaturesSetup.call( this );
